6d9dec8852023a0f80955485ab0ff4835db6b0fe
antnose <antnose28@gmail.com>
2026-02-11 02:07:31 +0600

    Create route for update product

--- rest/handlers/update_product.go
+++ rest/handlers/update_product.go
@@ -1,16 +1,37 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/antnose/Ecommerce/database"
+	"github.com/antnose/Ecommerce/util"
 )
 
 func UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	// productID := r.PathValue("id")
+	productID := r.PathValue("id")
 
-	// pId, err := strconv.Atoi(productID)
-	// if err != nil {
-	// 	http.Error(w, "Invalid product id", http.StatusNotFound)
-	// 	return
-	// }
+	pId, err := strconv.Atoi(productID)
+	if err != nil {
+		http.Error(w, "Invalid product id", http.StatusNotFound)
+		return
+	}
+
+	var newProduct database.Product
+
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&newProduct)
+
+	if err != nil {
+		http.Error(w, "Please provide a valid JSON", http.StatusBadRequest)
+		return
+	}
+
+	newProduct.ID = pId
+
+	database.Update(newProduct)
+
+	util.SendData(w, "Successfully updated product", 201)
 
 }
